74041cc6599211f092182f53b54cfbe56792d04f
Christopher Siegman <christopher.siegman@gmail.com>
2022-01-06 15:00:55 -0600

    Add Account Create view

--- bookkeeper/urls.py
+++ bookkeeper/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from bookkeeper.views import AccountListView
+from bookkeeper.views import AccountListView, AccountCreateView
 
 urlpatterns = [
     path('', AccountListView.as_view(), name='account-list'),
+    path('create/', AccountCreateView.as_view(), name='account-form'),
 ]
--- bookkeeper/views.py
+++ bookkeeper/views.py
@@ -1,9 +1,12 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
 
 from bookkeeper.models import Account
 
 
-class AccountListView(ListView):
+class AccountListView(LoginRequiredMixin, ListView):
 
     model = Account
 
@@ -12,3 +15,13 @@ class AccountListView(ListView):
         context['accounts'] = Account.objects.filter(user=self.request.user.pk)
         return context
 
+class AccountCreateView(LoginRequiredMixin, CreateView):
+    model = Account
+    fields = ['account_name', 'description', 'initial_balance', 'creation_date']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('account-list')
