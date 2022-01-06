6cc226053dd4838388be2f293b7f5f6c9ce4683f
Christopher Siegman <christopher.siegman@gmail.com>
2022-01-06 13:13:19 -0600

    Add description field to account/transaction models

--- bookkeeper/models.py
+++ bookkeeper/models.py
@@ -5,6 +5,7 @@ class Account(models.Model):
 
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     account_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
     creation_date = models.DateTimeField('creation date')
     initial_balance = models.DecimalField(max_digits=6, decimal_places=2)
 
@@ -18,6 +19,7 @@ class Transaction(models.Model):
     ]
 
     account = models.ForeignKey('Account', on_delete=models.CASCADE)    
+    description = models.CharField(max_length=50)
     amount = models.DecimalField(max_digits=6, decimal_places=2)
     transaction_date = models.DateTimeField('date')
 
