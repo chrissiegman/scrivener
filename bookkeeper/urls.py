3755c694a89d3d8ccd90478d0b5be653f4ce3e2a
Christopher Siegman <christopher.siegman@gmail.com>
2022-01-06 20:35:38 -0600

    Add AccountUpdateView

--- bookkeeper/urls.py
+++ bookkeeper/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from bookkeeper.views import AccountListView, AccountCreateView
+from bookkeeper.views import AccountListView, AccountCreateView, AccountUpdateView
 
 urlpatterns = [
     path('', AccountListView.as_view(), name='account-list'),
     path('create/', AccountCreateView.as_view(), name='create-account-form'),
+    path('edit/<pk>/', AccountUpdateView.as_view(), name='update-account-form'),
 ]
--- bookkeeper/views.py
+++ bookkeeper/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 
@@ -16,6 +16,18 @@ class AccountListView(LoginRequiredMixin, ListView):
         return context
 
 class AccountCreateView(LoginRequiredMixin, CreateView):
+    model = Account
+    fields = ['account_name', 'description', 'initial_balance', 'creation_date']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('account-list')
+
+
+class AccountUpdateView(LoginRequiredMixin, UpdateView):
     model = Account
     fields = ['account_name', 'description', 'initial_balance', 'creation_date']
 
