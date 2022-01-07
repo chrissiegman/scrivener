946f917112096d4ff9ad680ee5a79591dab336fe
Christopher Siegman <christopher.siegman@gmail.com>
2022-01-06 21:12:43 -0600

    Add TransactionUpdateView and TransactionDeleteView

--- bookkeeper/views.py
+++ bookkeeper/views.py
@@ -3,7 +3,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 
-from bookkeeper.models import Account
+from bookkeeper.models import Account, Transaction
 
 
 class AccountListView(LoginRequiredMixin, ListView):
@@ -40,6 +40,23 @@ class AccountUpdateView(LoginRequiredMixin, UpdateView):
 
 class AccountDeleteView(LoginRequiredMixin, DeleteView):
     model = Account
+
+    def get_success_url(self):
+        return reverse('account-list')
+
+class TransactionUpdateView(LoginRequiredMixin, UpdateView):
+    model = Transaction 
+    fields = ['description', 'amount', 'transaction_date', 'transaction_type']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('account-list')
+
+class TransactionDeleteView(LoginRequiredMixin, DeleteView):
+    model = Transaction 
 
     def get_success_url(self):
         return reverse('account-list')
